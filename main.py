06ba54086eef9c1e46c7b975648059dc55494539
NikolayMorgunov <nikolamorgunov@mail.ru>
2018-07-03 01:34:47 +1000

    Все "персонажи"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 from pygame import QUIT
-#from pygame.constants import K_LEFT, K_RIGHT, K_UP, K_SPACE, K_k, K_m
+from pygame.constants import K_UP, K_DOWN, K_RETURN
 from pygame.surface import Surface
 import sys
 import random
@@ -13,11 +13,24 @@ pygame.display.set_caption('Michael`s bottles')
 clock = Clock()
 
 class Hero:
-    place = (120, 240)
+    place = (95, 215)
+    v = 10
     def show(self):
         pygame.draw.ellipse(window, (0, 255, 0), [self.place[0], self.place[1], 25, 25])
 
+class Bottle:
+    place = hero.place
+    v = random.randint(-30, -10)
+    def show(self):
+        pygame.draw.rect(window, (255, 0, 0), [self.place[0], self.place[1], 25, 25])
 
+class Dildo:
+    place = (95, 215)
+    v = 15
+    def show(self):
+        pygame.draw.rect(window, (255, 255, 0), [self.place[0], self.place[1], 25, 25])
+
+fps = 0
 while True:
     for event in pygame.event.get():
         from pygame import QUIT
@@ -26,5 +39,7 @@ while True:
             sys.exit()
     hero = Hero()
     hero.show()
-
+    bottle = Bottle()
+    bottle.show()
+    fps = fps % 40
     clock.tick(60)
