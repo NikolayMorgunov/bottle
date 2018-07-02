b740364bc0b6ae138251d6c727ec19de5a098ff6
NikolayMorgunov <nikolamorgunov@mail.ru>
2018-07-02 23:58:35 +1000

    Основа

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import time
+#from pygame.constants import K_LEFT, K_RIGHT, K_UP, K_SPACE, K_k, K_m
+#from pygame.surface import Surface
+import sys
+import random
+
+clock = pygame.time.Clock()
+import pygame.time
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            sys.exit()
+            from pygame import QUIT
